c9d10d9db5f224e23b1f027179029a0c298f0587
Yusman Kamaleri <ybkamaleri@gmail.com>
2020-03-12 00:01:04 +0100

    test unsuccessful

--- saham.js
+++ saham.js
@@ -1,24 +1,31 @@
-// // saham.js
+// saham.js
 // var webPage = require('webpage');
 // var page = webPage.create();
 // var fs = require('fs');
+var page = require('webpage').create();
+var fs = require('fs')
 
-// page.open('https://www.oslobors.no/markedsaktivitet/#/list/shares/quotelist/ob/all/all/false', function (status) {
-//   fs.write('data/saham.html',page.content,'w')
-//   phantom.exit();
-// });
-
-
-// saham.js
-
-var webPage = require('webpage');
-var page = webPage.create();
-
-var fs = require('fs');
-var path = 'saham.html'
-
-page.open('http://www.techstars.com/companies/stats/', function (status) {
-  var content = page.content;
-  fs.write(path,content,'w')
+page.open('https://www.oslobors.no/markedsaktivitet/#/list/shares/quotelist/ob/all/all/false', function (status) {
+  console.log("Status: " + status);
+  if(status === "success"){
+  page.render('data/saham')
+  // fs.write('data/saham.html',page.content,'w')
+  }
   phantom.exit();
 });
+
+
+
+// // saham.js
+
+// var webPage = require('webpage');
+// var page = webPage.create();
+
+// var fs = require('fs');
+// var path = 'saham.html'
+
+// page.open('https://www.oslobors.no/markedsaktivitet/#/list/shares/quotelist/ob/all/all/false', function (status) {
+//   var content = page.content;
+//   fs.write(path,content,'w')
+//   phantom.exit();
+// });
